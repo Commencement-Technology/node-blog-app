67e2a183816f6b09a32473a488938590b81cdd9a
cobanalitalha <cobanalitalha@gmail.com>
2024-08-24 01:07:07 +0300

    Create verifyAuth middleware

---
+++ src/middlewares/verfiyAuth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader) {
+    return res
+      .status(401)
+      .json({ success: false, message: "No token provided" });
+  }
+
+  const token = authHeader.replace("Bearer ", "");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "No token provided" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid token",
+      error,
+    });
+  }
+};
